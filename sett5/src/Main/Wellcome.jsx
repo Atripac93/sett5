50fc80c988a2bbe10b026c5b696d40ce13dd783e
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-24 15:19:16 +0100

    Create Wellcome.jsx

---
+++ sett5/src/Main/Wellcome.jsx
@@ -0,0 +1,13 @@
+const Wellcome = () => {
+  return (
+    <>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt doloribus
+        atque ipsum distinctio perferendis, iusto officiis qui adipisci
+        pariatur, natus ad consectetur dignissimos harum, at labore. Laborum
+        tempora ab blanditiis?
+      </div>
+    </>
+  );
+};
+export default Wellcome;
