53f66727d87661ed12d5f17504bd13cdfb1016a8
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-24 15:19:07 +0100

    Create App.js

---
+++ sett5/src/App.js
@@ -0,0 +1,13 @@
+import Navigation from "./Nav/Navigation";
+import Wellcome from "./Main/Wellcome";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Wellcome />
+    </div>
+  );
+};
+
+export default App;
